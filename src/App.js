ac5c6fedff4caad3f647c85658f05af64f1cfca9
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2019-08-12 15:50:26 -0500

    Finished MVP requirements--moving on to stretch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -10,32 +13,26 @@ import ShoppingCart from './components/ShoppingCart';
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
+	
 
 	const addItem = item => {
 		setCart([...cart, item]);
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={{ cart }}>
+				<div className="App">
+					<Navigation />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+					{/* Routes */}
+					<Route exact path="/" component={ Products } />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+					<Route path="/cart" component={ ShoppingCart } />
+				</div>
+			</CartContext.Provider>
+		</ProductContext.Provider>
+
 	);
 }
 
